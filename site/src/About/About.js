2fdcaab61dcffa4b65a6bd490384326ecca9487d
csun <sunchang1265@gmail.com>
2018-07-18 16:46:02 -0400

    Adding About section

--- site/src/About/About.js
+++ site/src/About/About.js
@@ -8,11 +8,22 @@ class About extends React.Component {
             
             <div id="about">
                 <div className="left-section">
-                    <label>About</label>
+                    <h2>About</h2>
                     <img id="about-img" src={Img} />
                 </div>
                 <div className="right-section">
-                    Chang Sun
+                    <div id="self-intro">
+                        <h3>Chang Sun</h3>
+                        <p>Offering rich responsive user interface to help solve business problems is what I do best.</p>
+                        <p>Capable writing in JavaScript, CSS, also familiar with AngualarJS and ReactJS for web application front-end development.</p>
+                        <p>I also speak to databases and make servers do stuff with C# .NET and NodeJS with SQL or NoSQL databases.
+                           I like working on scalability, performance, reusability, and great user experiences.
+                        </p>
+                        <p> 
+                           As a full stack developer, I write code that conforms to industry standards and semantic best practices. 
+                           I am able to transform static artwork into pixel prefect, interactive and fully functional interfaces and capable doing all the back-end support as well.
+                        </p>
+                    </div>
                 </div>
             </div>
         );
--- site/webpack.config.js
+++ site/webpack.config.js
@@ -1,44 +1,57 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
+module.exports = env => {
+    console.log(env);
+    return {
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader"
+                    },
                 },
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader",
-                        options: {minimize: true }
-                    }
-                ],
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    "style-loader",
-                    "css-loader"
-                ],
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                    }
-                ],
-            }
+                {
+                    test: /\.html$/,
+                    use: [
+                        {
+                            loader: "html-loader",
+                            options: { minimize: true }
+                        }
+                    ],
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        "style-loader",
+                        "css-loader"
+                    ],
+                },
+                {
+                    test: /\.(png|svg|jpg|gif)$/,
+                    use: [
+                        {
+                            loader: "file-loader",
+                            options: {
+                                name(file) {
+                                    if (env.MODE === 'dev') {
+                                        return '[name].[ext]'
+                                    }
+
+                                    return '[hash].[ext]'
+                                },
+                                outputPath: 'images/'
+                            }
+                        }
+                    ],
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./src/index.html",
+                filename: "./index.html"
+            })
         ]
-    },
-    plugins:[
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-            filename: "./index.html"
-        })
-    ]
+    }
 };
